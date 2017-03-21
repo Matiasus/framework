fc48cfec0d190bd8b8d3a482aff98d86e33fb758
Marián <Matiasus@users.noreply.github.com>
2017-03-21 07:50:34 +0100

    Add files via upload

---
+++ Vendor/Reflection/reflection.class.php
@@ -0,0 +1,62 @@
+<?php
+
+/***
+* POZNAMKOVYBLOG Copyright (c) 2015 
+* 
+* Autor:      Mato Hrinko
+* Datum:      07.12.2016 / update
+* Adresa:     http://poznamkovyblog.cekuj.net
+* 
+* ------------------------------------------------------------
+* Description: 		
+*
+***/
+namespace Vendor\Reflection;
+
+class Reflection {
+
+  /** @var \ReflectionClass */
+  private $reflection;
+
+  /***
+   * Constructor
+   *
+   * @param Void
+   * @return Void
+   */
+  public function __construct()
+  {
+  }
+
+  /***
+   * Create controller
+   *
+   * @param Void
+   * @return Void
+   */
+  public function create($class = false)
+  {
+    // check if non empty value
+    if (func_num_args() > 1) {
+      // throw to exception with error message
+      throw new \Exception("[".get_called_class()."]:[".__LINE__."]: Too much arguments! Only <b>ONE</b> allowed!"); 
+    }    
+    // check if non empty value
+    if (empty($class)) {
+      // throw to exception with error message
+      throw new \Exception("[".get_called_class()."]:[".__LINE__."]: Class must be <b>NON</b> empty value!"); 
+    }
+    // check if string
+    if (!is_string($class)) {
+      // throw to exception with error message
+      throw new \Exception("[".get_called_class()."]:[".__LINE__."]: Class (namespace) must be <b>string</b>!"); 
+    }  
+    // 
+    if (!class_exists($class)) {
+      // throw to exception with error message
+      throw new \Exception('['.get_called_class().']:['.__LINE__.']: Class <b>'.$class.'</b> doesn't exist!');
+    }
+    // create new \ReflectionClass 
+    $this->reflection = new \ReflectionClass($class);  
+  }
+}
