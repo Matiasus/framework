45a0e6dc55a31247717f5e45180c910743e950b3
Marián <Matiasus@users.noreply.github.com>
2017-03-29 08:07:21 +0200

    Update form.class.php

--- Vendor/Form/form.class.php
+++ Vendor/Form/form.class.php
@@ -7,164 +7,155 @@ use \Vendor\Config\File as Config;
 
 class Form {
 
-  /** @const */
-	const GET = 'get';
-  /** @const */
-	const POST = 'post';
-  /** @const */
-	const REQUIRED = true;
-  /** @const */
-  const UNREQUIRED = false;
-
-	/** @var Object \Vendor\Form\Input */
+  /** @var Object \Vendor\Form\Input */
   private $input = null;
+
+  /** @var Array */
+  private $variables = array();
+  
+  /** @var Array */
+  private $params = array();
 
   /** @var String */
   private $code = '';
 
-  /** @var Array */
-  private $params = array();
-
-  /** @var Array */
-	private $variables = array();
-
-	/***
-	 * Constructor
-	 *
-	 * @param 
-	 * @return Void
-	 */
-	public function __construct()
+  /***
+  * Constructor
+  *
+  * @param 
+  * @return Void
+  */
+  public function __construct()
   {
     // @var \Vendor\Form\Input
     $this->input = new \Vendor\Form\Input($this);
-	}
+  }
 
-	/***
-	 * 
-	 *
-	 * @param 
-	 * @return Void
-	 */
-	public function input()
+  /***
+  * Return \Vendor\Form\Input
+  *
+  * @param  Void
+  * @return Void
+  */
+  public function input()
   {
     // return input
     return $this->input;
   }
 
-	/***
-	 * Set action type 
-   *
-	 * @param String
-	 * @return void
-	 */
-	public function setAction($action, $processing = false) 
+  /***
+  * Set action type 
+  *
+  * @param String
+  * @return void
+  */
+  public function setAction($action, $processing = false) 
   {
     // check if processing required
-		if($processing === false) {
+    if($processing === false) {
       // set action
-			$this->params['action'] = $action;
-		} else {
+      $this->params[Config::get('FORM', 'ACTION')] = $action;
+    } else {
       // set action with processing
-			$this->params['action'] = $action."?do=".$processing;
-		}
-	}
+      $this->params[Config::get('FORM', 'ACTION')] = $action."?".Config::get('ROUTE', 'PROCESS')."=".$processing;
+    }
+  }
 
-	/***
-	 * Get action
-   *
-	 * @param Void
-	 * @return String
-	 */
-	public function getAction()
+  /***
+  * Get action
+  *
+  * @param Void
+  * @return String
+  */
+  public function getAction()
   { 
     // check if set action
-    if (!empty($this->params['action'])) {
+    if (!empty($this->params[Config::get('FORM', 'ACTION')])) {
       // return set action
-      return $this->params['action'];
+      return $this->params[Config::get('FORM', 'ACTION')];
     }
     // action not set
     return false;
   }
 
-	/***
-	 * Set type of method
-   *
-	 * @param String GET || POST (default)
-	 * @return Void
-	 */
-	public function setMethod($method)
+  /***
+  * Set type of method
+  *
+  * @param String GET || POST (default)
+  * @return Void
+  */
+  public function setMethod($method)
   {
     // check if method exists
-		if (strcmp(strtolower($method), self::POST) !== 0 &&
-        strcmp(strtolower($method), self::GET)  !== 0)
-		{
+    if (strcmp(strtolower($method), strtolower(Config::get('FORM', 'METHOD_GET'))) !== 0 &&
+        strcmp(strtolower($method), strtolower(Config::get('FORM', 'METHOD_POST'))) !== 0)
+    {
       // error message
-      throw new \Exception('\\'.get_class($this).' -> '.ucfirst(__FUNCTION__).' ( ) [Line: '.__LINE__.']: Chosen form method <b>\''.$method.'\'</b> not exists!');
+      throw new \Exception('['.get_called_class().']:['.__LINE__.']: Chosen form method <b>\''.$method.'\'</b> not exists!');
     }
     // set method
-    $this->params['method'] = strtoupper($method);
-	}
+    $this->params[Config::get('FORM', 'METHOD')] = strtoupper($method);
+  }
 
-	/***
-	 * Get type of method
-   *
-	 * @param Void
-	 * @return String GET || POST (default)
-	 */
-	public function getMethod()
+  /***
+  * Get type of method
+  *
+  * @param  Void
+  * @return String GET || POST (default)
+  */
+  public function getMethod()
   { 
     // check if set method
-    if (!empty($this->params['method'])) {
+    if (!empty($this->params[Config::get('FORM', 'METHOD')])) {
       // return set method
-      return $this->params['method'];
+      return $this->params[Config::get('FORM', 'METHOD')];
     }
     // method not set
     return false;
   }
 
-	/***
-	 * Set inline form
-   *
-	 * @param Bool 
-	 * @return Void
-	 */
-	public function setInline($inline)
+  /***
+  * Set inline form
+  *
+  * @param Bool 
+  * @return Void
+  */
+  public function setInline($inline)
   {
     // check if method exists
-		if (!is_bool($inline)) {
+    if (!is_bool($inline)) {
       // error message
-      throw new \Exception('\\'.get_class($this).' -> '.ucfirst(__FUNCTION__).' ( ) [Line: '.__LINE__.']: Inline form <b>\''.$inline.'\'</b> not boolean!');
+      throw new \Exception('['.get_called_class().']:['.__LINE__.']: Inline form <b>\''.$inline.'\'</b> not boolean!');
     }
     // set inline form
-		$this->params['inline'] = $inline;
-	}
+    $this->params[Config::get('FORM', 'INLINE')] = $inline;
+  }
 
-	/***
-	 * Get type of inline method
-   *
-	 * @param Void
-	 * @return Bool
-	 */
-	public function getInline()
+  /***
+  * Get type of inline method
+  *
+  * @param Void
+  * @return Bool
+  */
+  public function getInline()
   { 
     // check if set inline method
-    if (!empty($this->params['inline'])) {
+    if (!empty($this->params[Config::get('FORM', 'INLINE')])) {
       // return set inline method
-      return $this->params['inline'];
+      return $this->params[Config::get('FORM', 'INLINE')];
     }
     // method not set
     return false;
   }
 
-	/***
-	 * 
-   *
-	 * @param  String
-	 * @return Void
-	 */
-	public function storeCode($code = array())
-	{
+  /***
+  * Store html codes
+  *
+  * @param  Array
+  * @return Void
+  */
+  public function storeCode($code = array())
+  {
     // check if non empty
     if (empty($code)) {
       // throw to exception with error message
@@ -177,122 +168,88 @@ class Form {
     }
     // store html code
     $this->code[array_keys($code)[0]] = array_values($code)[0];
-	}
+  }
 
-	/***
-	 * Get html code
-   *
-	 * @param Void
-	 * @return Void
-	 */
-	public function getCode()
-	{
+  /***
+  * Get html code
+  *
+  * @param Void
+  * @return Void
+  */
+  public function getCode()
+  {
     // form tag
-		$code  = "\n\t<form action='".$this->params[Config::get('FORM', 'ACTION')]."' method='".$this->params[Config::get('FORM', 'METHOD')]."' id=''>";
+    $code  = "\n\t<form action='".$this->params[Config::get('FORM', 'ACTION')];
+    $code .= "' method='".$this->params[Config::get('FORM', 'METHOD')]."' id=''>";
     // table tag
-		$code .= (($this->params[Config::get('FORM', 'INLINE')] === false ) ? "\n\t  <table class=''>":"");
-		// append all stored html code
-		foreach ($this->code as &$html_code) {
-      // appending
+    $code .= (($this->params[Config::get('FORM', 'INLINE')] === false ) ? "\n\t  <table class=''>":"");
+    // append all stored html codes
+    foreach ($this->code as &$html_code) {
+      // appending codes
       $code .= $html_code;
     }
     // unset
     unset($html_code);
     // end table tag
-		$code .= (($this->params[Config::get('FORM', 'INLINE')] === false ) ? "\n\t  </table>" : "");
+    $code .= (($this->params[Config::get('FORM', 'INLINE')] === false ) ? "\n\t  </table>" : "");
     // end form tag
-		$code .= "\n\t</form>";
+    $code .= "\n\t</form>";
     // return code
-		return $code;
-	}
+    return $code;
+  }
 
-	/***
-	 * Funkcia vracajuca obsah, ktory ma byt nahradeny v sablone {formular meno formulara}
-   *
-	 * @param String - id formulara
-	 * @return String - html, ktory bude nahradeny {formular meno_formulara}
-	 */
-	public function getFormContent( $id = false )
+  /***
+  * Validacia zadanych nazvov jednotlivych prvkov formulara
+  * ci sa zhoduju s nazvami stlpcov prislusnej tabulky
+  *
+  * @param void
+  * @return Bool
+  */		
+  public function succeedSend()
   {
-		$this->id = $id;
-		$form = $this->create();
-
-		return $form;
-	}
-
-	/***
-	 * Validacia zadanych nazvov jednotlivych prvkov formulara
-	 * ci sa zhoduju s nazvami stlpcov prislusnej tabulky
-   *
-	 * @param void
-	 * @return Bool
-	 */		
-	public function succeedSend()
-  {
-		if (isset($_POST) && 
+    if (isset($_POST) && 
         !empty($_POST))
-		{
+    {
       // overenie posielanych dat
-			$this->validation = call_user_func(array($this, "validation"));
+      $this->validation = call_user_func(array($this, "validation"));
       // navratova hodnota
-			return $this->validation;
-		}
+      return $this->validation;
+    }
+  }
 
-	}
-
-	/***
-	 * Overuje, ci sa nastavene $_POSTy zhoduju s nazvami stlpcov v tabulke
-   *
-	 * @param Void
-	 * @return Bool
-	 */		
-	private function validation($allerrors = false)
+  /***
+  * 
+  *
+  * @param Void
+  * @return Bool
+  */		
+  private function validation($allerrors = false)
   {
     // inicializacia chybovej hlasky
-		$this->registry->errors->sql = "";
-		// Prechod jednotlivych prvkov odoslanych metodou POST
-		foreach($_POST as $key => $value)	{
-			// Overenie existencie nayvu stlpca v MySQL tabulke databazy
-			if ($this->registry->mysql->existenceOfColumn($key) !== TRUE)
-			{
-				// Osetrenie submitu
-				if (!empty($this->submit->name)) {
-					if (strcmp($key, $this->submit->name) === 0) {
-						continue;
-					}
-				}
-				// Osetrenie checkboxu
-				if (!empty($this->checkbox->name)) {
-					if (strcmp($key, $this->checkbox->name) === 0) {
-						continue;
-					}
-				}
-				// Vypis flash spravy
-				$this->registry
-						 ->session
-						 ->set("flash", "Stlpec <strong>" . $key . "</strong> v tabulke <b>" . $this->registry->mysql->getTable() . "</b> neexistuje!<br/>", false);
-				// Ak najde stlpec, ktory sa v tabulke nenachadza vratti FALSE
-				return FALSE;
-			}
-			else
-			{
-				// Zapis udajov do pola data bez submit hodnoty
-				$this->data[$key] = $value;
-			}
-		}
-
-		return TRUE;
-	}
-
-	/***
-	 * Ziskanie dat
-   *
-	 * @param Void
-	 * @return Array
-	 */	
-	public function getData()
-  {
-		return $this->data;
-	}
-
+    $this->registry->errors->sql = "";
+    // Prechod jednotlivych prvkov odoslanych metodou POST
+    foreach($_POST as $key => $value)	{
+      // Overenie existencie nayvu stlpca v MySQL tabulke databazy
+      if ($this->registry->mysql->existenceOfColumn($key) !== TRUE)
+      {
+        // Osetrenie submitu
+        if (!empty($this->submit->name)) {
+          if (strcmp($key, $this->submit->name) === 0) {
+            continue;
+          }
+        }
+        // Osetrenie checkboxu
+        if (!empty($this->checkbox->name)) {
+          if (strcmp($key, $this->checkbox->name) === 0) {
+            continue;
+          }
+        }
+      } else {
+        // Zapis udajov do pola data bez submit hodnoty
+        $this->data[$key] = $value;
+      }
+    }
+    // return true
+    return true;
+  }
 }
