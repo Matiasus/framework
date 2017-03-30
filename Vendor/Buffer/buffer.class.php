a74e97085d964bd5e9e02186a919c88c2315baa1
Marián <Matiasus@users.noreply.github.com>
2017-03-30 07:56:07 +0200

    Update buffer.class.php

--- Vendor/Buffer/buffer.class.php
+++ Vendor/Buffer/buffer.class.php
@@ -3,12 +3,12 @@
 /***
 * POZNAMKOVYBLOG Copyright (c) 2016 
 * 
-* Autor:		Mato Hrinko
-*	Datum:		12.7.2015
-*	Adresa:		http://poznamkovyblog.cekuj.net
+* Autor:          Mato Hrinko
+* Datum:          12.7.2015
+* Adresa:         http://poznamkovyblog.cekuj.net
 * 
 * ------------------------------------------------------------
-* Inspiracia: 		
+* Description: 		
 *
 ***/
 namespace Vendor\Buffer;
@@ -16,56 +16,67 @@ namespace Vendor\Buffer;
 /** @var Class Buffer */
 class Buffer {
 
-	/** @var Object \Vendor\Errors\Errors */
-  private $errors;
-
   /** @var String */
-	private $path = '';
+  private $path = '';
+  
+  /** @var String */
+  private $buffer = '';  
 
   /***
-   * Constructor
-   *
-   * @param String
-   * @return Void
-   */
-	public function __construct() 
-	{
-	}
+  * Constructor
+  *
+  * @param String
+  * @return Void
+  */
+  public function __construct() 
+  {
+  }
 
   /***
-   * Set path to file
-   *
-   * @param String
-   * @return Void
-   */
-	public function setPath($path) 
-	{
+  * Set path to file
+  *
+  * @param String
+  * @return Void
+  */
+  public function setPath($path) 
+  {
     // path to file
     $this->path = $path;
   }
 
   /***
-   * Store buffered content
-   *
-   * @param Void
-   * @return Void
-   */
-	public function getBufferContent() 
-	{
-    // overenie existencie banky a dlzky nazvu
-    if (!isset($this->path)) {
+  * Store buffered content
+  *
+  * @param Void
+  * @return Void
+  */
+  public function getBufferContent() 
+  {
+    // is empty or not set?
+    if ( empty($this->path) || 
+        !isset($this->path)) {
       // throw to exception
-      throw new \Exception('\\'.get_class($this).' -> '.ucfirst(__FUNCTION__).' ( ) [Line: '.__LINE__.']: Can\'t buffer content from file <b>'.$this->path.'</b>');
+      throw new \Exception('\\'.get_class($this).'[Line: '.__LINE__.']: Path to file is empty! Can\'t  load file!');
+    }
+    // is string?
+    if (is_string($this->path)) {
+      // throw to exception
+      throw new \Exception('\\'.get_class($this).'[Line: '.__LINE__.']: Path to file must be string!');
+    }
+    // is readable?
+    if (!is_readable($this->path)) {
+      // throw to exception
+      throw new \Exception('\\'.get_class($this).'[Line: '.__LINE__.']: Can\'t read content from file <b>'.$this->path.'</b>. File is unreadable!');
     }
     // start buffering
-		ob_start();
+    ob_start();
     // include buffered file
-		require_once $this->path;
+    require_once $this->path;
     // buffered cntent
-		$buffer = ob_get_contents();
+    $this->buffer = ob_get_contents();
     // exit buffering
-		ob_end_clean();
+    ob_end_clean();
     // return buffered content
-    return $buffer;
+    return $this->buffer;
   }
 }
