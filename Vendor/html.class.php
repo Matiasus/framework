d01fc0528e1bb3c06c2d44aa522e89214a5ee647
Marián <Matiasus@users.noreply.github.com>
2017-03-30 09:33:41 +0200

    Create html.class.php

---
+++ Vendor/html.class.php
@@ -0,0 +1,36 @@
+<?php
+/***
+* POZNAMKOVYBLOG Copyright (c) 2015 
+* 
+* Autor:        Mato Hrinko
+* Datum:        30.03.2017
+* Adresa:       http://poznamkovyblog.cekuj.net
+* 
+* ------------------------------------------------------------
+* Description: 		
+*
+***/
+namespace Vendor\Html;
+
+class Html {
+
+  /***
+   * Constructor
+   *
+   * @param  Void
+   * @return Void
+   */
+  public function __construct ()
+  {
+  }
+  /***
+   * 
+   *
+   * @param  Void
+   * @return Void
+   */
+  public function create ($tag)
+  {
+  
+  }  
+}
