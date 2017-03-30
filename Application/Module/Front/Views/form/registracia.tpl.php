e600ace054509737e9d601e05e5aae0333ca4f2a
Marián <Matiasus@users.noreply.github.com>
2017-03-30 08:07:04 +0200

    Update registracia.tpl.php

--- Application/Module/Front/Views/form/registracia.tpl.php
+++ Application/Module/Front/Views/form/registracia.tpl.php
@@ -1,8 +1,8 @@
-	<div style="margin-right: 33%; margin:10px; margin-bottom: 20px;">
-		<h2 class="logon">Registrácia</h2>
-		{formular registracia}
-	</div>
+          <div style="margin-right: 33%; margin:10px; margin-bottom: 20px;">
+            <h2 class="logon">Registrácia</h2>
+            {formular registracia}
+          </div>
 
-	<div style="margin: 10px; padding-top: 10px; border-width: 1px 0px 0px 0px; border-style: solid; border-color: #ccc;">
-		alebo sa <a href="/front/form/default/" style="color: #08a1c3; font-weight: bold;">prihlás</a>!
-	</div>
+          <div style="margin: 10px; padding-top: 10px; border-width: 1px 0px 0px 0px; border-style: solid; border-color: #ccc;">
+            alebo sa <a href="/front/form/default/" style="color: #08a1c3; font-weight: bold;">prihlás</a>!
+          </div>
