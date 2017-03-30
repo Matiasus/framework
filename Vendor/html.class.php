7cc7471497555cde03335dc72ac2e960333199e4
Marián <Matiasus@users.noreply.github.com>
2017-03-30 09:35:21 +0200

    Update html.class.php

--- Vendor/html.class.php
+++ Vendor/html.class.php
@@ -29,8 +29,23 @@ class Html {
    * @param  Void
    * @return Void
    */
-  public function create ($tag)
+  public function create ($tag = false)
   {
-  
+    // check if non empty value
+    if (func_num_args() > 1) {
+      // throw to exception with error message
+      throw new \Exception("[".get_called_class()."]:[".__LINE__."]: Too much arguments! Only <b>1</b> argument allowed!"); 
+    }    
+    // check if non empty value
+    if (empty($tag)) {
+      // throw to exception with error message
+      throw new \Exception("[".get_called_class()."]:[".__LINE__."]: Tag must be <b>NON</b> empty!"); 
+    }
+    // check if string
+    if (!is_string($class)) {
+      // throw to exception with error message
+      throw new \Exception("[".get_called_class()."]:[".__LINE__."]: Tag must be a <b>string</b>!"); 
+    }
+    
   }  
 }
