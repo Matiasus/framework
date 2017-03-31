9b9fb35d935821aed457b98681a0d6ef274342ae
Marián <Matiasus@users.noreply.github.com>
2017-03-31 08:22:11 +0200

    Update html.class.php

--- Vendor/Html/html.class.php
+++ Vendor/Html/html.class.php
@@ -51,48 +51,27 @@ class Html {
    */
   public function __construct ()
   {
+    // @var \Vendor\Html\Attributes
+    $this->attributes = new \Vendor\Html\Attributes($this);
   }
   
+    
   /***
    * 
    *
-   * @param  Array
+   * @param  String
    * @return Void
    */
-  public static function setAttrs ($attributes = array())
+  public static function setAttrs ($attributes)
   {
-    // init attributes
-    $attributes = '';
-    // check if non empty attributes
-    if(!empty($attributes) && 
-        isset($attributes)) 
-    {
-      // check all elements of array
-      while (list($key, $value) = each($attributes)) {
-        // check if value is not array
-        if (!is_string($value) && 
-            !is_int($value)) 
-        {
-          // throw to exception with error message
-          throw new \Exception("[".get_called_class()."]:[".__LINE__."]: Value can\'t be array!");         
-        }
-        // if string
-        if (is_string($value)) {
-          // append attribute
-          $attributes .= ' '.$key.'=\''.$value.'\'';
-        // if integer
-        } elseif (is_int($value)) {
-          // append attribute
-          $attributes .= ' '.$key.'='.$value.'';
-        }        
-      }
-      // reset pointer
-      reset($attributes);
-      // set attributes
-      self::$tag_attributes = $attributes;
+    if (!is_string($attributes)) {
+      // throw to exception with error message
+      throw new \Exception("[".get_called_class()."]:[".__LINE__."]: Tag must be a <b>string</b>!");       
     }
-  }
-  
+    // return string form of attributes
+    $this->tag_attributes = $attributes;
+  }   
+    
   /***
    * 
    *
@@ -102,7 +81,7 @@ class Html {
   public static function getAttrs ()
   {
     // return string form of attributes
-    return self::$attributes;
+    return $this->attributes;
   }  
   
   /***
@@ -129,9 +108,9 @@ class Html {
       throw new \Exception("[".get_called_class()."]:[".__LINE__."]: Tag must be a <b>string</b>!"); 
     }
     // save tag
-    self::$tag = $tag;
+    $this->tag = $tag;
     // compose html tag
-    self::composer();
+    return $this->attributes;
   }
    
   /***
@@ -140,18 +119,18 @@ class Html {
    * @param  Void
    * @return Void
    */
-  private static function composer ()
+  public function compose ()
   {
     // init html code
-    self::$html_code = '<'.self::$tag;
+    $this->html_code = '<'.self::$tag;
     // if set close tags?
-    if (empty(self::$self_close_tags)) {
+    if (empty($this->self_close_tags)) {
       // loop throuh tags
-      foreach (self::$self_close_tags as &$self_close_tag) {
+      foreach ($this->self_close_tags as &$self_close_tag) {
         // compare with created tag
-        if (strcmp($self_close_tag, self::$tag) === 0) {
+        if (strcmp($self_close_tag, $this->tag) === 0) {
           // close tag
-          self::$html_code .= ''.self::$getAttributes().' />';
+          $this->html_code .= ''.$this->getAttributes().' />';
           // end loop
           return true;
         }
@@ -159,7 +138,7 @@ class Html {
       // unset variable
       unset($self_close_tag);
       // append html code
-      self::$html_code .= ' '.self::$getAttributes().'>'.self::$tag_content.'</'.self::$tag.'>';
+      $this->html_code .= ' '.$this->getAttributes().'>'.$this->tag_content.'</'.$this->tag.'>';
       // success return
       return true;
     }
