2cac738de89107e52559d0ea6674d23590059cef
Marián <Matiasus@users.noreply.github.com>
2017-03-31 08:18:19 +0200

    Update attributes.class.php

--- Vendor/Html/attributes.class.php
+++ Vendor/Html/attributes.class.php
@@ -28,4 +28,44 @@ class Attributes {
     // @var \Vendor\Html\Html $html
     $this->html = $html;
   }
+  
+  /***
+   * 
+   *
+   * @param  Array
+   * @return Void
+   */
+  public static function attributes ($attributes = array())
+  {
+    // init attributes
+    $tag_attributes = '';
+    // check if non empty attributes
+    if(!empty($attributes) && 
+        isset($attributes)) 
+    {
+      // check all elements of array
+      while (list($key, $value) = each($attributes)) {
+        // check if value is not array
+        if (!is_string($value) && 
+            !is_int($value)) 
+        {
+          // throw to exception with error message
+          throw new \Exception("[".get_called_class()."]:[".__LINE__."]: Value can\'t be array!");         
+        }
+        // if string
+        if (is_string($value)) {
+          // append attribute
+          $tag_attributes .= ' '.$key.'=\''.$value.'\'';
+        // if integer
+        } elseif (is_int($value)) {
+          // append attribute
+          $tag_attributes .= ' '.$key.'='.$value.'';
+        }        
+      }
+      // reset pointer
+      reset($attributes);
+      // set attributes
+      $this->html->setAttrs($tag_attributes);
+    }
+  }  
 }
