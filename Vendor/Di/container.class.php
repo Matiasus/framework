9b1f3c75794817204a5065ff13688b091ce7664b
Marián <Matiasus@users.noreply.github.com>
2017-04-02 14:20:33 +0200

    Add files via upload

--- Vendor/Di/container.class.php
+++ Vendor/Di/container.class.php
@@ -13,13 +13,11 @@
 ***/
 namespace Vendor\Di;
 
+
 class Container {
 
   /** @var */
   private $reflection;
-
-  /** @var */
-  private $services = array();
 
   /***
    * Constructor
@@ -42,33 +40,7 @@ class Container {
    */
   public function store ($class = false, $arguments = false)
   {
-    // check if non empty value
-    if (func_num_args() > 2) {
-      // throw to exception with error message
-      throw new \Exception("[".get_called_class()."]:[".__LINE__."]: Too much arguments! Only <b>TWO</b> argument possible!"); 
-    }    
-    // check if non empty value
-    if (empty($class)) {
-      // throw to exception with error message
-      throw new \Exception("[".get_called_class()."]:[".__LINE__."]: Key must be <b>NON</b> empty value!"); 
-    }
-    // check if string
-    if (!is_string($class)) {
-      // throw to exception with error message
-      throw new \Exception("[".get_called_class()."]:[".__LINE__."]: Key must be a <b>string</b>!"); 
-    }
-    // if class doesn't exist
-    if (!class_exists($class)) {
-      // throw to exception with error message
-      throw new \Exception("[".get_called_class()."]:[".__LINE__."]: Class <b>'$class'</b> doesn't exists!"); 
-    }
-    // if non empty arguments
-    if (!empty($arguments)) {
-      // throw to exception with error message
-      $this->reflection->setArguments($arguments);
-    }
-    // return reuired instance of class
-    return $this->services[$class] = $this->reflection->resolve($class);
+    return $this->reflection->setInstance($class, $arguments);
   }
 
   /***
@@ -77,30 +49,9 @@ class Container {
    * @param  String
    * @return Void
    */
-  public function service ($name = false)
+  public function service ($class = false, $error = true)
   {
-    // check if non empty value
-    if (func_num_args() > 1) {
-      // throw to exception with error message
-      throw new \Exception("[".get_called_class()."]:[".__LINE__."]: Too much arguments! Only <b>ONE</b> argument possible!"); 
-    }    
-    // check if non empty value
-    if (empty($name)) {
-      // throw to exception with error message
-      throw new \Exception("[".get_called_class()."]:[".__LINE__."]: Key must be <b>NON</b> empty value!"); 
-    }
-    // check if string
-    if (!is_string($name)) {
-      // throw to exception with error message
-      throw new \Exception("[".get_called_class()."]:[".__LINE__."]: Key must be a <b>string</b>!"); 
-    }
-    // if class doesn't exist
-    if (!array_key_exists($name, $this->services)) {
-      // throw to exception with error message
-      throw new \Exception("[".get_called_class()."]:[".__LINE__."]: Class <b>'$name'</b> doesn't exists!"); 
-    }
-    // return service
-    return $this->services[$name];
+    return $this->reflection->getInstance($class, $error);
   }
 }
 
